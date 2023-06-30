51eb1ca666bd7ea6df1544715b559b081abbc216
MaggieWesolowska <maggiewes07@gmail.com>
2023-06-30 15:39:20 +0200

    export default added to App

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -8,10 +8,26 @@ import css from './ContactForm/ContactForm.module.css';
 export const App = () => {
   const [filter, setFilter] = useState('');
   const [contacts, setContacts] = useState([
-    { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
-    { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
-    { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
-    { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
+    {
+      id: 'id-1',
+      name: 'Rosie Simpson',
+      number: '459-12-56',
+    },
+    {
+      id: 'id-2',
+      name: 'Hermione Kline',
+      number: '443-89-12',
+    },
+    {
+      id: 'id-3',
+      name: 'Eden Clements',
+      number: '645-17-79',
+    },
+    {
+      id: 'id-4',
+      name: 'Annie Copeland',
+      number: '227-91-26',
+    },
   ]);
 
   useEffect(() => {
@@ -22,7 +38,10 @@ export const App = () => {
   }, []);
 
   useEffect(() => {
-    localStorage.setItem('contactList', JSON.stringify(contacts));
+    localStorage.setItem(
+      'contactList',
+      JSON.stringify(contacts)
+    );
   }, [contacts]);
 
   const handleChange = e => {
@@ -36,7 +55,11 @@ export const App = () => {
     const number = e.number;
     const contactList = [...contacts];
 
-    if (contactList.findIndex(contact => name === contact.name) !== -1) {
+    if (
+      contactList.findIndex(
+        contact => name === contact.name
+      ) !== -1
+    ) {
       alert(`${name} is already in contacts.`);
     } else {
       contactList.push({ name, id, number });
@@ -45,12 +68,16 @@ export const App = () => {
   };
 
   const handleDelete = e => {
-    setContacts(contacts.filter(contact => contact.id !== e));
+    setContacts(
+      contacts.filter(contact => contact.id !== e)
+    );
   };
 
   const getFilteredContacts = () => {
     const filteredContactList = contacts.filter(contact => {
-      return contact.name.toLowerCase().includes(filter.toLowerCase());
+      return contact.name
+        .toLowerCase()
+        .includes(filter.toLowerCase());
     });
     return filteredContactList;
   };
@@ -67,8 +94,7 @@ export const App = () => {
         background: '#D9DDDc',
         margin: 20,
         borderRadius: 5,
-      }}
-    >
+      }}>
       <h1 className={css.header}>Phonebook📱</h1>
       <ContactForm handleSubmit={handleSubmit} />
       <h2 className={css.contacts}>Contacts</h2>
@@ -80,3 +106,5 @@ export const App = () => {
     </div>
   );
 };
+
+export default App;
